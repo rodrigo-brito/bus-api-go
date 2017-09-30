407c3f3c4b55dda8e627d81ba76d6c32a92f0f51
Rodrigo Brito <rig.10@hotmail.com>
2017-09-30 00:05:49 -0300

    cors adapter

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	routes "github.com/rodrigo-brito/bus-api-go/action/cli/server/http"
 	_ "github.com/rodrigo-brito/bus-api-go/config"
 	"github.com/rodrigo-brito/bus-api-go/lib/mysql"
+	"github.com/rs/cors"
 	"github.com/spf13/viper"
 )
 
@@ -16,6 +17,8 @@ func main() {
 	defer mysql.CloseConnection()
 	router := routes.InjectAPIRoutes()
 
+	handler := cors.Default().Handler(router)
+
 	fmt.Printf("Server started at http://localhost:%d\n", viper.GetInt("port"))
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", viper.GetInt("port")), router))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", viper.GetInt("port")), handler))
 }
