abb697fc4d8acb4262ef6cc655b6b1ff1173982c
Rodrigo Brito <rig.10@hotmail.com>
2017-10-01 17:48:48 -0300

    Async mail

--- action/http/api/contact.go
+++ action/http/api/contact.go
@@ -14,12 +14,12 @@ func ContactHandle(w http.ResponseWriter, r *http.Request, _ httprouter.Params) 
 	name := r.FormValue("name")
 	message := r.FormValue("message")
 
-	err := mail.SendMessage(email, "Contato - Onibus Sabara (WEB)", name, message)
-	if err != nil {
-		glog.Error(err)
-		w.WriteHeader(http.StatusServiceUnavailable)
-		return
-	}
+	go func() {
+		err := mail.SendMessage(email, "Contato - Onibus Sabara (WEB)", name, message)
+		if err != nil {
+			glog.Error("api/contact: ", err)
+		}
+	}()
 
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-type", "application/json; charset=utf-8")
