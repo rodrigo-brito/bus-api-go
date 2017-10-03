572f0ac2f94fa43c0f65187c81eca79f527aa36f
Rodrigo Brito <rig.10@hotmail.com>
2017-10-02 21:18:51 -0300

    Change email payload parser

--- action/http/api/contact.go
+++ action/http/api/contact.go
@@ -3,19 +3,38 @@ package api
 import (
 	"net/http"
 
+	"encoding/json"
+	"io/ioutil"
+
 	"github.com/golang/glog"
 	"github.com/julienschmidt/httprouter"
 	"github.com/rodrigo-brito/bus-api-go/lib/mail"
 )
 
+type Message struct {
+	Name    string `json:"name"`
+	Email   string `json:"email"`
+	Message string `json:"message"`
+}
+
 func ContactHandle(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	r.ParseForm()
-	email := r.Form.Get("email")
-	name := r.Form.Get("name")
-	message := r.Form.Get("message")
+	b, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	message := new(Message)
+	err = json.Unmarshal(b, message)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
 
 	go func() {
-		err := mail.SendMessage(email, "Contato - Onibus Sabara (WEB)", name, message)
+		err := mail.SendMessage(message.Email, "Contato - Onibus Sabara (WEB)",
+			message.Name, message.Message)
 		if err != nil {
 			glog.Error("api/contact: ", err)
 		}
