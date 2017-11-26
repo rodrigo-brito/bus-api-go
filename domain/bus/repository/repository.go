1248817e72865076b2249a7b32ee16849250a453
Rodrigo Brito <rig.10@hotmail.com>
2017-11-26 18:01:59 -0200

    Remove debug and update gitignore

--- domain/bus/repository/repository.go
+++ domain/bus/repository/repository.go
@@ -51,7 +51,6 @@ func Get(ctx context.Context, ID int64, injectSchedule bool) (*model.Bus, error)
 	key := getBusCacheKey(ID)
 
 	err := cache.GetSet(key, bus, func() (interface{}, error) {
-		fmt.Println("No CACHE", queries["by-id"])
 		rows, err := db.Query(queries["by-id"], ID)
 		if err != nil {
 			return nil, err
